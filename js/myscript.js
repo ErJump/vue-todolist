bb4195754aedb3fe8adf64c79095a7776ac36fa6
ErJump <fedelijunior@gmail.com>
2022-07-06 13:06:40 +0200

    Aggiunge i todos nel DOM tramite vue

---
+++ js/myscript.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            },
+            {
+                text: 'Fare il bravo ragazzo',
+                done: true
+            },
+            {
+                text: 'Vedere i Gorillaz',
+                done: true
+            },
+            {
+                text: 'Diventare un pro gamer di LoL',
+                done: false
+            },
+            {
+                text: 'Imparare a programmare',
+                done: true
+            },
+        ],
+    },
+})
